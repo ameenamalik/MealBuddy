f05db7456a658ddd1dc2d3f4f12a57f7b381f401
Ameena <ameenazara3@gmail.com>
2023-11-29 16:08:15 -0500

    mealbuddy expo

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './src/amplifyconfiguration.json';
+
+Amplify.configure(amplifyconfig);
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Ops to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
